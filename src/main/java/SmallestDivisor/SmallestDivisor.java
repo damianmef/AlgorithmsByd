39c27bd35cffca9a879c8b8430cb18bd3f02d72f
Damian <damianszkudlarek@gmail.com>
2017-08-06 14:24:20 +0200

    + smallest divisor

---
+++ src/main/java/SmallestDivisor/SmallestDivisor.java
@@ -0,0 +1,35 @@
+package SmallestDivisor;
+
+import java.util.Scanner;
+
+public class SmallestDivisor {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Enter two integer values: ");
+        int userInputA = scanner.nextInt();
+        int userInputB = scanner.nextInt();
+        System.out.println(getSmallestDivisor(userInputA, userInputB));
+
+    }
+
+    private static int getSmallestDivisor(int a, int b) {
+
+        while (a != b) {
+
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+
+        return a;
+
+    }
+}
+
+
