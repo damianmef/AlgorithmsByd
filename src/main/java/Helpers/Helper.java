f0ba2d54980890d4459cc126ecc5a3a420cbd101
Damian <damianszkudlarek@gmail.com>
2017-08-19 12:11:50 +0200

    + Eratostenes

--- src/main/java/Helpers/Helper.java
+++ src/main/java/Helpers/Helper.java
@@ -20,6 +20,23 @@ public class Helper {
         return  arrayN;
     }
 
+    public static int[] getArrayFromUserBySize()
+    {
+        System.out.println("Podaj wielkość tablicy:");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] arrayN = new int[n-1];
+
+        for(int i = 0; i < arrayN.length;i++) {
+            arrayN[i] = i+2;
+        }
+
+        return arrayN;
+    }
+
+
+
     public static void printArray(int[] arrayN) {
         for (int i = 0; i < arrayN.length; i++) {
             System.out.println(arrayN[i]);
